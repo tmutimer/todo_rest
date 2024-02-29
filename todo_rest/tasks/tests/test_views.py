5b46b55198899d52d82aaea729de648d17c90eb1
Tom Mutimer <tmutimer96@gmail.com>
2024-02-29 13:49:14 +0000

    Feat: Auth - emaisl must be unique

--- todo_rest/tasks/tests/test_views.py
+++ todo_rest/tasks/tests/test_views.py
@@ -4,6 +4,9 @@ from rest_framework.test import APITestCase
 
 class AuthTests(APITestCase):
     def test_user_registration(self):
+        '''
+        Test that a user can register.
+        '''
         url = reverse('register')
         data = {'email': 'test@user.com', 'password': 'Password1!'}
         response = self.client.post(url, data, format='json')
@@ -11,11 +14,14 @@ class AuthTests(APITestCase):
         self.assertTrue('token' in response.data)
 
     def test_user_registration_conflicting_email(self):
+        '''
+        Test that a user cannot register with an email that already exists.
+        '''
         url = reverse('register')
         data = {'email': 'helloagain@user.com', 'password': 'Password1!'}
         first_response = self.client.post(url, data, format='json')
         self.assertEqual(first_response.status_code, status.HTTP_201_CREATED)
         second_response = self.client.post(url, data, format='json')
         print(second_response.data)
-        # We already used that email address for an existing user, so it's a conflict
-        self.assertEqual(second_response.status_code, status.HTTP_409_CONFLICT)
+        # We already used that email address for an existing user, so it's a bad request.
+        self.assertEqual(second_response.status_code, status.HTTP_400_BAD_REQUEST)
