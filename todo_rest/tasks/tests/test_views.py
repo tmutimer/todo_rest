e55d8019fcdeefafcc8a1f54d95bc898145abf17
Tom Mutimer <tmutimer96@gmail.com>
2024-02-29 17:29:15 +0000

    test: invalid email

--- todo_rest/tasks/tests/test_views.py
+++ todo_rest/tasks/tests/test_views.py
@@ -3,6 +3,16 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 
 class AuthTests(APITestCase):
+
+    def create_user(self, email, password):
+        '''
+        Create a user for testing.
+        '''
+        url = reverse('register')
+        data = {'email': email, 'password': password}
+        response = self.client.post(url, data, format='json')
+        return response
+
     def test_user_registration(self):
         '''
         Test that a user can register.
@@ -32,6 +42,13 @@ class AuthTests(APITestCase):
         response = self.create_user('', 'Password1!')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
+    def test_user_registration_invalid_email(self):
+        '''
+        Test that a user cannot register with an invalid email.
+        '''
+        response = self.create_user('invalidemail', 'Password1!')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
     def test_user_registration_no_password(self):
         '''
         Test that a user cannot register without a password.
@@ -53,13 +70,3 @@ class AuthTests(APITestCase):
 
         response = self.create_user('nospecial@user.com', 'Password11')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        
-
-    def create_user(self, email, password):
-        '''
-        Create a user for testing.
-        '''
-        url = reverse('register')
-        data = {'email': email, 'password': password}
-        response = self.client.post(url, data, format='json')
-        return response
